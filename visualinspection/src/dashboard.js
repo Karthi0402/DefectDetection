6d2aa992bb846ff69bcedf879d466802f3e7e87b
Karthi0402 <karthikgarapati123@gmail.com>
2023-02-28 21:51:44 +0530

    3rd

--- visualinspection/src/dashboard.js
+++ visualinspection/src/dashboard.js
@@ -1,13 +1,36 @@
 import React from 'react'
 import logo from './Img/logo.jpeg';
+import './CSS/dash.css';
+import { AiOutlineUser } from 'react-icons/ai';
+import { IconContext } from 'react-icons';
 const Dashboard = () => {
-  return (
-    <div>
-        <div className='NavBar'>
-        <img className='PHOTO' src={logo} alt="Ari-Logo" />
+    return (
+        <div>
+            <div className='NavBar'>
+                <img className='PHOTO' src={logo} alt="Ari-Logo" />
+                <IconContext.Provider value={{ className: "Icon1", size: "2em" }}>
+                    <AiOutlineUser />
+                </IconContext.Provider>
+                <span style={{ "color": "black" }}>Profile</span>
+            </div>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <td><h3>Visual Inspection</h3></td>
+                            <td><button type='submit'>Log Details</button></td>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+            <div className='GRID'>
+                <div className='TopLeft' >tl</div>
+                <div className='TopRight'>tr</div>
+                <div className='BottomLeft'>bl</div>
+                <div className='BottomRight'>br</div>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Dashboard
