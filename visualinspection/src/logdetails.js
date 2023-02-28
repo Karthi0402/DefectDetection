52e1c8e1c3b0e5fb3b89f089565db79f53479abd
Karthi0402 <karthikgarapati123@gmail.com>
2023-02-28 22:53:51 +0530

    4tj

--- visualinspection/src/logdetails.js
+++ visualinspection/src/logdetails.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import logo from './Img/logo.jpeg';
+import './CSS/log.css';
+import { AiOutlineUser } from 'react-icons/ai';
+import { IconContext } from 'react-icons';
 const Logdetails = () => {
   return (
     <div>
-        <div className='NavBar'>
+      <div className='NavBar'>
         <img className='PHOTO' src={logo} alt="Ari-Logo" />
-        </div>
+        <IconContext.Provider value={{ className: "Icon1", size: "2em" }}>
+          <AiOutlineUser />
+        </IconContext.Provider>
+        <span style={{ "color": "black" }}>Profile</span>
+      </div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <td><h3>Log Details</h3></td>
+              <td><button type='submit'>Close</button></td>
+            </tr>
+          </thead>
+        </table>
+      </div>
     </div>
   )
 }
