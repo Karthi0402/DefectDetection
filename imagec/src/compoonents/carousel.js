d3660df7403240dbfa276295fa1e27cff8c07e97
Karthi0402 <karthikgarapati123@gmail.com>
2023-03-14 10:47:59 +0530

    i

---
+++ imagec/src/compoonents/carousel.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './carousel.css';
+const carousel = () => {
+  return (
+    <div className='carousel'>
+        <div className='carouselinner'> 
+            
+        </div>
+    </div>
+  )
+}
+
+export default carousel
