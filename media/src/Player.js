28eb067b04c7a4fcd2db43997ddebe2f014c0157
Karthi0402 <karthikgarapati123@gmail.com>
2023-03-15 11:55:29 +0530

    77

--- media/src/Player.js
+++ media/src/Player.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactAudioPlayer from 'react-audio-player';
 import pawan from './assets/pspk.mp3';
+import music from './assets/music.png';
 import File from './Files';
-// import { file1 } from './play';
+import { file1 } from './play';
 const Player = () => {
   return (
     <div> 
         <File/>
         <br/>
-        
+        {/* <img src={music} style={{}}/> */}
+        <center><div style={{width:"20%",height:"250px",backgroundColor:"black",backgroundImage:`url`}}>
+        </div></center>
+        <br/>
         <ReactAudioPlayer 
-        src={pawan}
+        src={file1}
         autoPlay
         controls
         />
