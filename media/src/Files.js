19947788dfccad24024bc0ee2f4227c0cff6f75c
Karthi0402 <karthikgarapati123@gmail.com>
2023-03-15 11:31:50 +0530

    33

---
+++ media/src/Files.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { file1 } from './play'
+const Files = () => {
+  return (
+    <div>
+      <h1>Upload a File</h1>
+      {file1}
+    </div>
+  )
+}
+
+export default Files
+
--- media/src/Player.js
+++ media/src/Player.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactAudioPlayer from 'react-audio-player';
-
+// import pawan from './assets/pspk.mp3';
+import File from './Files';
+import { file1 } from './play';
 const Player = () => {
   return (
-    <div>
+    <div> 
+        <File/>
         <ReactAudioPlayer 
-        src='pawan.mp3'
+        src={file1}
         autoPlay
         controls
         />
