3e010eafad56beed2b21c6ecc4528987fb95b7ee
Karthi0402 <karthikgarapati123@gmail.com>
2023-03-16 11:45:38 +0530

    ss

--- media/src/Player.js
+++ media/src/Player.js
@@ -1,28 +1,42 @@
 import React, { useState } from 'react';
 import ReactAudioPlayer from 'react-audio-player';
 import music from './assets/music.png';
+import axios from 'axios';
 const Player = () => {
-  const [audio,setAudio] = useState();
- const handleFile=()=>{
-    console.log(audio);
+  const [audio, setAudio] = useState();
+  const [play,setPlay] = useState();
+  const handleInput=(e)=>{
+    setAudio(e.target.files[0])
+    console.log(e.target.files)
+  }
+  const handleSubmit=(e)=>{
+    const formData = new FormData()
+    formData.append("audio",audio)
+    axios.post('http://localhost:3000/upload',formData).then((res)=>{
+      setPlay(res)
+      console.log(play)
+    })
   }
   return (
-    <div> 
-        <div>
-      <h1 style={{fontFamily:"times",color:"white"}}>Select a song</h1>
-      <input type="file" src='http://127.0.0.1:8887/' value={audio} onChange={e=>setAudio(e.target.value)} onClick={handleFile} style={{fontFamily:"times",color:"white"}}/>
-    </div>
-        <br/>
-        <center><div style={{width:"20%",height:"250px",backgroundColor:"black"}}>
-        <div style={{height:"100%",width:"100%",backgroundPosition:"center",backgroundRepeat:"no-repeat",backgroundSize:"cover",display:"flex",backgroundImage:`url(${music})`}}></div>
-        </div></center>
-        <br/>
-        <ReactAudioPlayer 
-        src= {audio}
+    <div>
+      <div>
+        
+        <h1 style={{ fontFamily: "times", color: "white" }}>Select a song</h1>
+        <input type="file" name='file'  onChange={handleInput} style={{ fontFamily: "times", color: "white" }} />
+        <button onClick={handleSubmit}>Upload</button>
+    
+      </div>
+      <br />
+      <center><div style={{ width: "20%", height: "250px", backgroundColor: "black" }}>
+        <div style={{ height: "100%", width: "100%", backgroundPosition: "center", backgroundRepeat: "no-repeat", backgroundSize: "cover", display: "flex", backgroundImage: `url(${music})` }}></div>
+      </div></center>
+      <br />
+      <ReactAudioPlayer
+        src={play}
         type='audio/mp3'
         autoPlay
         controls
-        />
+      />
     </div>
   )
 }
